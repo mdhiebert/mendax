5c488c83d2ba7cb720114707cff5be7ccf884c06
mdhiebert <hiebertmd@gmaill.com>
2024-06-23 17:30:57 -0400

    Added logs

--- mendax/__init__.py
+++ mendax/__init__.py
@@ -6,6 +6,13 @@ from types import SimpleNamespace
 
 from mendax.util.hackrf.core import receive_to_file_for_time, transmit_from_file
 
+logging.basicConfig(
+    filename='./logs/mendax.log',
+    level = logging.DEBUG,
+    format = '[%(asctime)s] %(message)s',
+    datefmt='%m-%d-%Y %H:%M:%S'
+)
+
 class MendaxState(enum.Enum):
     IDLE = 0
     SLEEPING = 1
@@ -43,13 +50,16 @@ class Mendax:
         if not os.path.exists('./data/tmp'):
             os.mkdir('./data/tmp')
 
+        if not os.path.exists('./logs'):
+            os.mkdir('./logs')
+
     def run(self):
         while self.should_live:
             self._loop_cycle()
     
     def _loop_cycle(self):
         try:
-            print(f'In state {self.state}')
+            logging.debug(f'In state {self.state}')
             self.state_function_map[self.state]()
         except Exception as e:
             self.state_parameters.crash_buffer = str(e)
@@ -72,8 +82,8 @@ class Mendax:
     
     def _idle_state(self):
         # TODO handle state transitions
-        print(f'Handling IDLE state')
-        # print(f'Awaiting user input...')
+        logging.debug(f'Handling IDLE state')
+        # logging.debug(f'Awaiting user input...')
 
         if self.state_parameters.future_state_queue:
             self.state = self.state_parameters.future_state_queue.pop(0)
@@ -86,19 +96,19 @@ class Mendax:
 
     def _sleeping_state(self):
         # TODO implement
-        print(f'Handling SLEEPING state')
-        print(f'Timing out...')
+        logging.debug(f'Handling SLEEPING state')
+        logging.debug(f'Timing out...')
 
     def _observing_state(self):
         '''
             This is the state where the Mendax is listening to signals.
         '''
-        print(f'Handling OBSERVING state')
+        logging.debug(f'Handling OBSERVING state')
 
-        print(f'Initiating HackRF receive for 30s with following parameters:')
-        print(f'Center Frequency: {self.state_parameters.center_freq_hz}')
-        print(f'Sample Rate: {self.state_parameters.sample_rate_hz}')
-        print(f'Duration: {self.state_parameters.duration_s}')
+        logging.debug(f'Initiating HackRF receive for 30s with following parameters:')
+        logging.debug(f'Center Frequency: {self.state_parameters.center_freq_hz}')
+        logging.debug(f'Sample Rate: {self.state_parameters.sample_rate_hz}')
+        logging.debug(f'Duration: {self.state_parameters.duration_s}')
 
         output_file = receive_to_file_for_time(
             self.state_parameters.center_freq_hz,
@@ -106,11 +116,11 @@ class Mendax:
             duration_s=self.state_parameters.duration_s
         )
 
-        print(f'File saved to {output_file}')
+        logging.debug(f'File saved to {output_file}')
 
         self.state_parameters.last_observation_file = output_file
 
-        print(f'Observation complete, transitioning to THINKING state...')
+        logging.debug(f'Observation complete, transitioning to THINKING state...')
 
         self.state = MendaxState.THINKING
 
@@ -120,26 +130,26 @@ class Mendax:
 
             At this time, we will simply log that we are processing signals. TODO.
         '''
-        print(f'Handling THINKING state')
-        print(f'Processing signals...')
+        logging.debug(f'Handling THINKING state')
+        logging.debug(f'Processing signals...')
 
         if self.state_parameters.broadcast_immediately:
-            print(f'Broadcasting immediately...')
-            print(f'Transitioning to BROADCASTING state...')
+            logging.debug(f'Broadcasting immediately...')
+            logging.debug(f'Transitioning to BROADCASTING state...')
             self.state = MendaxState.BROADCASTING
         else:
-            print(f'Transitioning to IDLE state...')
+            logging.debug(f'Transitioning to IDLE state...')
             self.state = MendaxState.IDLE
 
     def _broadcasting_state(self):
         # TODO implement
-        print(f'Handling BROADCASTING state')
-        print(f'Sending signals...')
+        logging.debug(f'Handling BROADCASTING state')
+        logging.debug(f'Sending signals...')
 
-        print(f'Initiating HackRF transmit with following parameters:')
-        print(f'Center Frequency: {self.state_parameters.center_freq_hz}')
-        print(f'Sample Rate: {self.state_parameters.sample_rate_hz}')
-        print(f'Input File: {self.state_parameters.last_observation_file}')
+        logging.debug(f'Initiating HackRF transmit with following parameters:')
+        logging.debug(f'Center Frequency: {self.state_parameters.center_freq_hz}')
+        logging.debug(f'Sample Rate: {self.state_parameters.sample_rate_hz}')
+        logging.debug(f'Input File: {self.state_parameters.last_observation_file}')
 
         transmit_from_file(
             self.state_parameters.center_freq_hz,
@@ -148,49 +158,49 @@ class Mendax:
         )
 
         if self.state_parameters.broadcast_forever:
-            print(f'Looping...')
+            logging.debug(f'Looping...')
             self.state = MendaxState.BROADCASTING
         else:
-            print(f'Broadcast complete, transitioning to IDLE state...')
+            logging.debug(f'Broadcast complete, transitioning to IDLE state...')
             self.state = MendaxState.IDLE
 
     def _crash_state(self):
         # TODO implement
-        print(f'Handling CRASH state')
-        print(f'Crashed with error: {self.state_parameters.crash_buffer}')
+        logging.debug(f'Handling CRASH state')
+        logging.debug(f'Crashed with error: {self.state_parameters.crash_buffer}')
 
         match self.state_parameters.crash_buffer:
             case 'timeout':
-                print(f'Attempting to recover...')
+                logging.debug(f'Attempting to recover...')
                 self.state = MendaxState.RECOVER
             case 'user interrupt':
-                print(f'Preparing to die...')
+                logging.debug(f'Preparing to die...')
                 self.state = MendaxState.DYING
             case _:
-                print(f'Unknown error, dying...')
+                logging.debug(f'Unknown error, dying...')
                 self.state = MendaxState.DYING
 
     def _recover_state(self):
         # TODO implement
-        print(f'Handling RECOVER state')
-        print(f'Recovering from crash...')
+        logging.debug(f'Handling RECOVER state')
+        logging.debug(f'Recovering from crash...')
 
-        print(f'Reinitializing things...')
+        logging.debug(f'Reinitializing things...')
 
         self.state = MendaxState.IDLE
 
     def _dying_state(self):
         # TODO implement
-        print(f'Handling DYING state')
-        print(f'Preparing to die...')
+        logging.debug(f'Handling DYING state')
+        logging.debug(f'Preparing to die...')
 
-        print(f'Cleaning up...')
+        logging.debug(f'Cleaning up...')
 
         self.state = MendaxState.DEAD
 
     def _dead_state(self):
         # TODO implement
-        print(f'Handling DEAD state')
-        print(f'Already dead...')
+        logging.debug(f'Handling DEAD state')
+        logging.debug(f'Already dead...')
 
         self.should_live = False
